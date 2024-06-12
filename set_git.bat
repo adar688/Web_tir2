abce0840116eea0429a69aad6e00c76884a56e48
Adar Budomski <adar688@gmail.com>
2024-06-12 20:17:37 +0300

    Wed 06-12-24

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/[set your github repository path].git
+git remote add origin https://github.com/adar688/Web_tir2.git
 git push -u origin main
